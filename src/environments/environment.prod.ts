863529ac4bd8da6e47e9ff8de45e6760e30adf22
Thawatchai Saengduan <thawatchai.sea2@gmail.com>
2021-03-05 18:06:22 +0700

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3022',
-  docUrl: 'http://localhost:3023/uploads'
+  apiUrl: '/api/smart-tshospha',
+  docUrl: '/uploads/smart-tshospha'
 };
